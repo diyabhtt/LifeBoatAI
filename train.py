7647152fafcc9f6f0166a45ecb61fa71e8ac77f3
Ansh Sharma <sharmaansh1705@gmail.com>
2025-02-01 16:57:43 -0600

    HI

--- train.py
+++ train.py
@@ -6,6 +6,6 @@ import tensorflow_decision_forests as tfdf
 print(f"Found TF-DF {tfdf.__version__}")
 
 
-train_df = pd.read_csv("/Users/diyabhattarai/Kaggle/input/titanic/train.csv")
-train_df = pd.read_csv("/Users/diyabhattarai/Kaggle/input/titanic/test.csv")
+train_df = pd.read_csv("C:\Users\sharm\Documents\LifeBoat-AI\train.csv")
+test_df = pd.read_csv("C:\Users\sharm\Documents\LifeBoat-AI\test.csv")
 print(train_df.head(10))
