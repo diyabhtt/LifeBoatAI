f28559e64b064d1b3cf78f26b12e8a935302e81d
Diya Bhattarai <101464852+diyabhtt@users.noreply.github.com>
2025-02-01 16:01:16 -0600

    hi

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import tensorflow_decision_forests as tfdf
 print(f"Found TF-DF {tfdf.__version__}")
 
+
 train_df = pd.read_csv("/Users/diyabhattarai/Kaggle/input/titanic/train.csv")
 train_df = pd.read_csv("/Users/diyabhattarai/Kaggle/input/titanic/test.csv")
 print(train_df.head(10))
